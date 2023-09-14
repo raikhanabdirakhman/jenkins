7e84a9a3167cc3881101c2d5c374899fe779d0a8
raikhanabdirakhman <136049922+raikhanabdirakhman@users.noreply.github.com>
2023-09-13 19:13:04 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+region = "var.region"
+}
+resource "aws_instance" "web" {
+ami = var.ami_id
+instance_type = var.instance_type
+availability_zone = var.az1
+key_name = var.key_name
+}
