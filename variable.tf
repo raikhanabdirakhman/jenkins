b3e06f16fcedefde6b7a43b27bdbee769efe101f
raikhanabdirakhman <136049922+raikhanabdirakhman@users.noreply.github.com>
2023-09-13 19:14:00 -0500

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,20 @@
+variable ami_id {
+    type = string
+    default = ""
+}
+variable instance_type {
+    type = string
+    default = ""
+}
+variable az1 {
+    type = string
+    default = ""
+}
+variable key_name {
+    type = string
+    default = ""
+}
+variable region {
+    type = string
+    default = ""
+}
