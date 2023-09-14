2e9617a7d41746d8605b76322c1bc193f2f5ddcf
raikhanabdirakhman <136049922+raikhanabdirakhman@users.noreply.github.com>
2023-09-13 19:14:52 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-region = "var.region"
+region = var.region
 }
 resource "aws_instance" "web" {
 ami = var.ami_id
 instance_type = var.instance_type
 availability_zone = var.az1
 key_name = var.key_name
-}
+} (
